286d21da8eae5eaa4937e1196eac5e1b46b4c879
drs251 <danielstephan@Davis.local>
2017-08-22 02:18:43 +0200

    Plugin architecture works

--- plugin_importer.py
+++ plugin_importer.py
@@ -1,11 +1,29 @@
 import os
+import importlib
+import collections
+
+plugin_folder = "plugins"
+
 
 def find_plugins():
-    res = []
-    for file in os.listdir("./plugins"):
+    plugins = collections.OrderedDict()
+    print(os.getcwd())
+    for file in os.listdir("./" + plugin_folder):
         if file.endswith(".py"):
-            plugin = __import__("plugins." + os.path.splitext(file)[0])
-            print("imported", plugin)
+            name = os.path.splitext(file)[0]
+            plugin = importlib.import_module(plugin_folder + "." + name)
+            if plugin is None:
+                continue
+            try:
+                plugins[name] = plugin.process_frame
+            except AttributeError:
+                pass
+
+    return plugins
+
 
 if __name__ == "__main__":
-    find_plugins()
+    my_plugins = find_plugins()
+    print(my_plugins)
+    for name, plugin in my_plugins.items():
+        plugin(None)
