344e6be8d4350823eda762ad11a51bfda24a8da8
drs251 <danielstephan@Davis.local>
2017-08-27 22:25:26 +0200

    New dummy plugin and plugin_canvas

---
+++ plugin_canvas.py
@@ -0,0 +1,35 @@
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+
+
+class PluginCanvas(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # set the layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+        self.plot()
+
+    def set_name(self, name):
+        self.setWindowTitle(name)
+
+    def show_canvas(self):
+        self.show()
+        self.raise_()
+        self.activateWindow()
--- plugins/dummy1.py
+++ plugins/dummy1.py
@@ -1,73 +1,35 @@
+import plugin_canvas
+import random
 
 name = "Dummy 1"
 
 description= "A dummy plugin"
 
-import sys
-from PyQt5 import QtGui, QtWidgets
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
-
-import random
-
-class Window(QtWidgets.QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        # a figure instance to plot on
-        self.figure = plt.figure()
-
-        # this is the Canvas Widget that displays the `figure`
-        # it takes the `figure` instance as a parameter to __init__
-        self.canvas = FigureCanvas(self.figure)
-
-        # this is the Navigation widget
-        # it takes the Canvas widget and a parent
-        self.toolbar = NavigationToolbar(self.canvas, self)
-
-        # Just some button connected to `plot` method
-        self.button = QtWidgets.QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
-
-        # set the layout
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
-
-    def plot(self):
-        ''' plot some random stuff '''
-        # random data
-        data = [random.random() for i in range(10)]
-
-        # create an axis
-        ax = self.figure.add_subplot(111)
-
-        # discards the old graph
-        ax.hold(False)
-
-        # plot data
-        ax.plot(data, '*-')
-
-        # refresh canvas
-        self.canvas.draw()
+canvas = None
 
 
 def init(parent=None):
-    window = Window(parent)
-    window.show()
-    window.raise_()
-    window.activateWindow()
-    pass
+    global canvas
+    canvas = plugin_canvas.PluginCanvas(parent)
+    canvas.set_name(name)
+    canvas.show_canvas()
 
 
 def process_frame(frame):
-    app = QtWidgets.QApplication(sys.argv)
+    ''' plot some random stuff '''
+    # random data
+    data = [random.random() for i in range(10)]
 
-    main = Window()
-    main.show()
+    # create an axis
+    ax = canvas.figure.add_subplot(111)
 
-    sys.exit(app.exec_())
+    # discards the old graph
+    ax.hold(False)
+
+    # plot data
+    ax.plot(data, '*-')
+
+    # refresh canvas
+    canvas.canvas.draw()
+
+    canvas.show_canvas()
