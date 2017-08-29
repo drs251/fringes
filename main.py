55e22cf76e3fef8b84e5b5d04e3dc72472f383ef
drs251 <danielstephan@Davis.local>
2017-08-30 01:12:15 +0200

    Experiment with a receiver object in main.py -> unsuccessful

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import sys
 from os import path
 
 import PyQt5
-from PyQt5.QtCore import QObject, QUrl, QCoreApplication, QVariant
+from PyQt5.QtCore import QObject, QUrl, QCoreApplication, QVariant, pyqtSlot
 from PyQt5.QtGui import QGuiApplication, QIcon
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQml import qmlRegisterType, QQmlComponent, QQmlEngine, QQmlApplicationEngine
-from PyQt5.QtMultimedia import QVideoProbe, QCamera
+from PyQt5.QtMultimedia import QVideoProbe, QCamera, QVideoFrame
 
 from plugin_loader import PluginLoader
 
+class dummyProbe(QObject):
+
+    def __init__(self, probe=None):
+        super().__init__(self)
+        self._probe = probe
+
+    #@pyqtSlot(QVideoFrame)
+    def processFrame(self, frame):
+        print(frame)
 
 app = QApplication(sys.argv)
 app.setWindowIcon(QIcon('fringes.png'))
@@ -28,8 +37,11 @@ if not probe.setSource(camera):
     print("Could not install video probe!")
 else:
     pluginloader = root.findChild(QObject, "pluginloader")
-    probe.videoFrameProbed.connect(pluginloader.processFrame)
-    print(probe.isActive())
+    print("connect to video probe:", probe.videoFrameProbed.connect(pluginloader.processFrame))
+    print("probe active: ", probe.isActive())
+
+    dummy = dummyProbe()
+    print("dummy probe connect:", probe.videoFrameProbed.connect(dummy.processFrame))
 
 
 sys.exit(app.exec_())
